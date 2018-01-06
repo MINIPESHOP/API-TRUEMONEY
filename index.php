9c3e59d7b7c443f2da735af352728ae57a8b6ce9
Kittsak Sakmueng <32167774+MINIPESHOP@users.noreply.github.com>
2018-01-06 18:08:15 +0700

    API One File

---
+++ index.php
@@ -0,0 +1,25 @@
+  <div class="container">
+    <div class="col-md-4 col-md-offset-4">
+        <div class="panel panel-default">
+            <div class="panel-body">
+  <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel='stylesheet' id='rs-settings-css'  href='css/style.css' type='text/css' media='all' />
+<link rel="stylesheet" href="css/bootstrap.css">
+<script type="text/javascript" src='https://www.tmtopup.com/topup/3rdTopup.php?uid=78838'></script>
+</head>
+<body>
+        <div class="form-group">
+                        <label for="username"><span class="glyphicon glyphicon-credit-cart"></span> รหัสบัตรทรูมันนี่14หลัก</label>
+                        <input id="tmn_password" maxlength="14" name="tmn_password" type="text" placeholder="รหัสบัตรทรูมันนี่...">
+                    </div>
+                    <div class="form-group">
+                        <label for="password"><span class="glyphicon glyphicon-user"></span> ชื่อในเกม</label>
+                        <input name="text" name="ref1" id="ref1" placeholder="ชื่อในเกม...">
+                    </div>
+                    <div class="form-group">
+                        <label for="email"><span class="glyphicon glyphicon-envelope"></span> </label>
+                        <input type="text" name="ref2" id="ref2" placeholder="">
+                    </div>
+                            <input type="button" value="เติมเงิน" class="button" onclick="submit_tmnc()"/>
